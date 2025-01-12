6a12e15a7f023e37ce6cdbcfa8bdf306ad799e43
Iskandar Hakimi Zulkippli <iskandarzulkippli@heitech.com.my>
2025-01-13 04:53:45 +0800

    UI for Program's Status Form
    - Update dashboard page for School Admin and TVPSS Officer
    - Update sidebar and it's navigation for several pages

--- src/main/java/com/codecrafters/tvpss/controller/ProgramStatusController.java
+++ src/main/java/com/codecrafters/tvpss/controller/ProgramStatusController.java
@@ -1,45 +1,33 @@
 package com.codecrafters.tvpss.controller;
 
 import com.codecrafters.tvpss.model.ProgramStatusModel;
-import com.codecrafters.tvpss.model.ResourceRequestModel;
-import com.codecrafters.tvpss.model.TalentApplicationModel;
-import com.codecrafters.tvpss.service.ResourceRequestService;
+import com.codecrafters.tvpss.service.ProgramStatusService;
+
+import java.util.List;
+import java.util.Map;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ProgramStatusController {
 
     @Autowired
-    private ResourceRequestService resourceRequestService;
+    private ProgramStatusService programStatusService;
 
-    @GetMapping("/program-status-form")
+    @GetMapping("/submit-status")
     public String showForm(Model model) {
-        model.addAttribute("programStatusRequest", new ProgramStatusModel());
-        return "school-program-status-form";
-    }
-
-    @GetMapping("/manage-program-status-list")
-    public String showManageProgramStatusList(Model model) {
-        model.addAttribute("programStatusRequest", new ProgramStatusModel());
-        return "manage-school-program-status";
-    }
-
-    @GetMapping("/program-status-list")
-    public String showProgramStatusList(Model model) {
-        model.addAttribute("programStatusRequest", new ProgramStatusModel());
-        return "school-program-status-list";
-    }
-
-    @GetMapping("/program-status-report")
-    public String showProgramStatusReport(Model model) {
-        model.addAttribute("programStatusRequest", new ProgramStatusModel());
-        return "school-program-status-report";
+        model.addAttribute("submitStatus", new ProgramStatusModel());
+        return "programs'-status/programs'-status-form";
     }
 
 }
---
+++ src/main/java/com/codecrafters/tvpss/dao/ProgramStatusDao.java
@@ -0,0 +1,4 @@
+package com.codecrafters.tvpss.dao;
+
+public class ProgramStatusDao {
+}
